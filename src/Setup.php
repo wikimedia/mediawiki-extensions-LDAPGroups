ea1fdf6fb6ce2d4f82081342bd4537f51794a231
rvogel <vogel@hallowelt.biz>
2017-11-24 07:42:49 +0100

    Started work

--- src/Setup.php
+++ src/Setup.php
@@ -4,6 +4,6 @@ namespace MediaWiki\Extension\LDAPGroups;
 
 class Setup {
 	public static function onRegistration() {
-		die( \MediaWiki\MediaWikiServices::getInstance()->getConfigFactory()->get( Config::GROUP_MAP_FILE ) );
+
 	}
 }
