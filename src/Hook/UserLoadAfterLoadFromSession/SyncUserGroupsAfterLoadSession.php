68a198252fbf8eb67763611f83f8cce2bc365b18
oleksandrpinchuk <intracomof@gmail.com>
2019-04-16 20:01:08 +0300

    UserGroupSync Hook after LoadSession
    Based on abstract LDAPProvider/UserLoadAfterLoadFromSession
    Hook that allows to manage frequency of sync process
    
    Deleted UserLoggedIn Hook
    
    Fixed Codestyle issues
    
    Change-Id: I217a51a4b63396e4e73f302b3e3abd61542c2724
    Task: T220471

---
+++ src/Hook/UserLoadAfterLoadFromSession/SyncUserGroupsAfterLoadSession.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MediaWiki\Extension\LDAPGroups\Hook\UserLoadAfterLoadFromSession;
+
+use MediaWiki\Extension\LDAPProvider\Hook\UserLoadAfterLoadFromSession;
+use MediaWiki\Extension\LDAPGroups\GroupSyncProcess;
+use MediaWiki\Extension\LDAPGroups\Config;
+
+class SyncUserGroupsAfterLoadSession extends UserLoadAfterLoadFromSession {
+
+	protected $sessionDataKey = 'ldap-group-sync-last';
+
+	/**
+	 * @return bool
+	 * @throws \ConfigException
+	 */
+	protected function doSync() {
+		$process = new GroupSyncProcess(
+			$this->user,
+			$this->domainConfig,
+			$this->ldapClient,
+			$this->config->get( 'LDAPGroupsSyncMechanismRegistry' )
+		);
+
+		$process->run();
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function getDomainConfigSection() {
+		return Config::DOMAINCONFIG_SECTION;
+	}
+}
